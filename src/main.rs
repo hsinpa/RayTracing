7c9e8cd031fd4572cac1b4f442cad718d851c835
Hsinpa <hsinpa@gmail.com>
2022-05-26 10:11:59 +0800

    Draw blue texture

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+use pixels::{Error, Pixels, SurfaceTexture};
+
+const WIDTH: u32 = 320;
+const HEIGHT: u32 = 240;
+const BOX_SIZE: i16 = 64;
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    let window_size = window.inner_size();
+    let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+
+    let mut pixels = Pixels::new(WIDTH, HEIGHT, surface_texture).unwrap();
+
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+            Event::RedrawRequested(_) => {
+                let frame = pixels.get_frame();
+                for pixel in frame.chunks_exact_mut(4) {
+                    pixel[0] = 0; // R
+                    pixel[1] = 0; // G
+                    pixel[2] = 255; // B
+                    pixel[3] = 255; // A
+                }
+                pixels.render();
+            },
+            _ => (),
+
+
+        }
+    });
 }
