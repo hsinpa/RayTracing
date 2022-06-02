4d6a471207d7b97ba582353ef0ffab609a16207f
Hsinpa <hsinpa@gmail.com>
2022-06-02 14:40:13 +0800

    Add random vector

--- src/utility/utility_func.rs
+++ src/utility/utility_func.rs
@@ -1,3 +1,4 @@
+use cgmath::{InnerSpace, Vector3};
 use rand::Rng;
 use rand::rngs::ThreadRng;
 
@@ -19,6 +20,26 @@ impl UtilityFunc {
 
     pub fn get_random_range(&mut self, min: f32, max: f32) -> f32 {
         min + (max - min) * self.get_random_float()
+    }
+
+    pub fn random_vector3(&mut self) -> Vector3<f32> {
+        return Vector3::new(self.get_random_float(),self.get_random_float(), self.get_random_float());
+    }
+
+    pub fn random_vector3_rangge(&mut self, min: f32, max: f32) -> Vector3<f32> {
+        return Vector3::new(self.get_random_range(min, max),self.get_random_range(min, max), self.get_random_range(min, max));
+    }
+
+    pub fn random_in_unit_sphere(&mut self) -> Vector3<f32> {
+        loop {
+            let p = self.random_vector3_rangge(-1.0, 1.0);
+
+            if (p.magnitude() >= 1.0) {
+                continue;
+            }
+            return p;
+
+        }
     }
 
     pub fn degrees_to_radians(degrees: f32) -> f32 {
